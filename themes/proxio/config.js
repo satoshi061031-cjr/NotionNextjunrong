cd1a6235e1829f1f0d4f533bec581e1226a433d4
satoshi061031-cjr <satoshi061031@gmail.com>
2026-04-07 18:01:21 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -18,7 +18,12 @@ const CONFIG = {
   PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github-mark.svg', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  PROXIO_HERO_BANNER_IMAGE: '', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
+  PROXIO_HERO_BANNER_IMAGE
+:
+'/images/cover.jpg'
+,
+
+ // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
   PROXIO_HERO_BANNER_IFRAME_URL: '', // hero背景区内嵌背景网页 ，可以配置一个网页地址，例如动画网页https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/
 
   // 文章区块
